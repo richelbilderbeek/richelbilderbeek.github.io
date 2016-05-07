cc33152079cf3d7471d173d1a57746de2a25d74b
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-07 08:53:18 +0200

    Bye LonierPong folder

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ wget https://github.com/richelbilderbeek/LonelierPong/archive/master.zip
 unzip master.zip
 rm master.zip
 mv LonelierPong-master LonelierPong
+cd LonelierPong; rm .gitignore; cd ..
 cp LonelierPong.html LonelierPong/
