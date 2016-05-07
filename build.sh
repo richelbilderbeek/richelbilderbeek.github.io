d93170d45f4a1c2a33c33391319ab090637104bd
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-07 09:08:58 +0200

    Update

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 wget https://github.com/richelbilderbeek/LonelierPong/archive/master.zip
 unzip master.zip
 rm master.zip
+rm -rf LonelierPong
 mv LonelierPong-master LonelierPong
 cd LonelierPong; rm .gitignore; cd ..
 cp LonelierPong.html LonelierPong/
+cp RichelbilderbeekNlBackground.png LonelierPong/
